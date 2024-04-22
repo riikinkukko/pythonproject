79ea738bca1ee43707a0a2e9c1ba016cb8e97d12
riikinkukko <119848942+riikinkukko@users.noreply.github.com>
2024-04-22 17:03:00 +0300

    Update settings.py

--- supersite/settings.py
+++ supersite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-5ep*$+2y*%@lm2l!$j6=m5*9+wm*0fi5(!&c4s18g9ju))7m9q
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [".vercel.app"]
+ALLOWED_HOSTS = ['127.0.0.1', '.vercel.app']
 
 
 # Application definition
